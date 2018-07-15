ea5342c93962ce078ecdece036597745ade24feb
CurryPseudo <jesonxiake@126.com>
2018-07-15 16:16:02 +0800

    Not sovled location score yet

--- src/AI/State/Board/Internal.hs
+++ src/AI/State/Board/Internal.hs
@@ -13,20 +13,28 @@ import Data.Array.Utils
 import Board
 import AI.State
 
-data BoardState = BS {score :: !Int, states :: !ArrayState, bestChoice :: ![(Pos, Pos)]} deriving (Eq, Show)
+data BoardState = BS {score :: !Int, states :: !ArrayState, bestChoice :: !([Pos], [Pos])} deriving (Eq, Show)
 type ArrayState = Array (Int, Int) DirStates 
 data Dir = Horizontal | Vertical | MainDiag | Diag deriving (Show, Eq, Ord)
-type DirStates = (Int, M.Map Dir State, (Int, Int))
+type DirStates = (Int, M.Map Dir State, [Int])
+boardElem :: DirStates -> Int
+boardElem (e, _, _) = e
+
+toDirStateMap :: DirStates -> M.Map Dir State
+toDirStateMap (_, dsm, _) = dsm
+
+toScoresMap :: DirStates -> [Int]
+toScoresMap (_, _, sm) = sm
 type Pos = (Int, Int)
 type Size = (Int, Int)
+
 
 empty :: (Int, Int) -> Int -> BoardState
 empty (w, h) bestCount = BS 0 as choice where
     as = genTableArray (w, h) $ buildDirStates (emptyBoard w h)
     ie = assocs as
-    ns f (_, _, s) = f s 
-    sorted f = map fst $ sortBy (comparing (ns f . snd)) ie
-    choice = zip (take bestCount (sorted fst)) (take bestCount (reverse (sorted snd)))
+    sorted f = map fst $ sortOn (f . toScoresMap . snd) ie
+    choice = (take bestCount (sorted (!! 1)),take bestCount (reverse (sorted (!! 2))))
 
 fromBoard :: Board -> Int -> BoardState
 fromBoard b bestCount = let
@@ -40,8 +48,7 @@ buildDirStates b pos = let
     insertDir (d, vec) = M.insert d (dirState vec)
     rMap = foldr insertDir M.empty dirVecs
     re = getElem b pos
-    cScore c = dirStatesTransScore re rMap c + locationScore (getSize b) pos * chessRltSign re c
-    in (re, rMap, (cScore 1, cScore 2))
+    in (re, rMap, scoreCombine rMap (getSize b) pos re)
 
 dirStatesTransScore :: Int -> M.Map Dir State -> Int -> Int
 dirStatesTransScore ce m e = let
@@ -49,15 +56,32 @@ dirStatesTransScore ce m e = let
     deltaScores = map deltaStateScore (M.elems m) 
     in sum deltaScores
 
+scoreCombine :: M.Map Dir State -> Size -> Pos ->  Int -> [Int]
+scoreCombine dsm s pos ce = flip map [0..2] $ \c -> dirStatesTransScore ce dsm c + locationScore s pos * chessRltSign ce c
+
+
 update :: BoardState -> PosElem -> BoardState
 update bs@(BS sc sts choices) pe@(p, e) = let
-    (ce, baseDirStates, (best, worst)) = sts ! p 
-    score = dirStatesTransScore ce baseDirStates e + locationScore (getSize sts) p * chessRltSign ce e
+    (ce, baseDirStates, scoreMap) = sts ! p 
+    deltaScore = scoreMap !! e
     (w, h) = getSize sts
     ndUdtPoses = udtedStatesPoses w h p
-    udtAsFuncs = flip map ndUdtPoses $ \ pos -> mapAsUdtStates pos (udtStatesBase pos pe)
-    nsts = foldr (\f x -> f x) sts udtAsFuncs // [(p, (e, baseDirStates))]
-    in BS (sc + score) nsts
+    scoreTrans p (e, dsm, _) = (e, dsm, scoreCombine dsm (w, h) p e)
+    udtAsFuncs = flip map ndUdtPoses $ \ pos -> mapAsUdtStates pos (scoreTrans pos . udtStatesBase pos pe)
+    nsts = foldr (\f x -> f x) sts udtAsFuncs // [(p, (e, baseDirStates, scoreCombine baseDirStates (w, h) p e))]
+    emptyPoses = filter (\pos -> boardElem (sts ! pos) == 0) ndUdtPoses
+    nChoices = foldr (insertBestChoices nsts) choices emptyPoses
+    in BS(sc + deltaScore) nsts nChoices
+
+insertBestChoices :: ArrayState -> Pos -> ([Pos], [Pos]) -> ([Pos], [Pos])
+insertBestChoices as p before = (inserts (!! 1) bests, inserts (!! 2) worsts) where
+    takeScore f p = f scores where
+        (_, _, scores) = as ! p
+    inserts f = insertNotIncreaseCount (comparing (takeScore f)) p
+    (bests, worsts) = before
+
+insertNotIncreaseCount :: (a -> a -> Ordering) -> a -> [a] -> [a]
+insertNotIncreaseCount comp x xs = take (length xs) $ insertBy comp x xs
 
 udtedStatesPoses :: Int -> Int -> Pos -> [Pos]
 udtedStatesPoses w h p@(x, y) = let
@@ -80,17 +104,18 @@ udtedStatesPosesUnFilter p = flip concatMap dirVecs $ \(_, d) ->
     dirSeq p d
 
 udtStatesBase :: Pos -> PosElem -> DirStates -> DirStates
-udtStatesBase pos (bp, be) ds = let
+udtStatesBase pos (bp, be) (e, m, scoreMap) = let
     Just (dir, statePos) = rltvDirPos pos bp
     fds :: M.Map Dir State -> M.Map Dir State
     fds = M.adjust (stateTrans statePos be) dir
-    in mapSnd fds ds
+    in (e, fds m, scoreMap)
 
 mapAsUdtStates :: Pos -> (DirStates -> DirStates) -> ArrayState -> ArrayState
 mapAsUdtStates pos f as = as // [(pos, f (as ! pos))]
 
 mapSnd :: (b -> b) -> (a, b) -> (a, b)
 mapSnd f (x, y) = (x, f y)
+
 
 doPos2 :: (Int -> Int -> Int) -> Pos -> Pos -> Pos
 doPos2 f (x1, y1) (x2, y2) = (f x1 x2, f y1 y2)
--- test/AI/State/BoardSpec.hs
+++ test/AI/State/BoardSpec.hs
@@ -9,52 +9,9 @@ import Board
 
 spec :: Spec
 spec = 
-    describe "fromBoard" $
-        it "should return a board state and score from a board" $ do
-            let sb = score . fromBoard . fromJust
-            let
-                b1 = listBoard [ [0, 0, 0, 0, 0]
-                               , [0, 1, 1, 0, 0]
-                               , [0, 0, 0, 0, 0]
-                               , [0, 0, 0, 0, 0]
-                               ]
-                b2 = listBoard [ [0, 0, 0, 0, 0]
-                               , [0, 0, 1, 0, 0]
-                               , [0, 0, 1, 0, 0]
-                               , [0, 0, 0, 0, 0]
-                               ]   
-                in compare (sb b1) (sb b2) `shouldBe` LT
-            
-            let 
-                b1 = listBoard [ [0, 0, 0, 0, 0]
-                               , [0, 2, 0, 0, 0]
-                               , [0, 1, 1, 0, 0]
-                               , [0, 1, 0, 1, 0]
-                               , [0, 0, 0, 0, 0]
-                               ]
-                b2 = listBoard [ [0, 0, 0, 0, 0]
-                               , [0, 0, 0, 0, 0]
-                               , [0, 1, 1, 0, 0]
-                               , [0, 1, 2, 1, 0]
-                               , [0, 0, 0, 0, 0]
-                               ]
-                in compare (sb b1) (sb b2) `shouldBe` LT
-            let 
-                b1 = listBoard [ [0, 0, 0, 0, 0, 0, 0]
-                               , [0, 1, 2, 0, 0, 0, 0]
-                               , [0, 1, 0, 2, 0, 0, 0]
-                               , [0, 1, 0, 0, 2, 0, 0]
-                               , [0, 1, 0, 0, 0, 2, 0]
-                               , [0, 1, 0, 0, 0, 0, 0]
-                               ]
-                b2 = listBoard [ [0, 0, 0, 0, 0, 0, 0]
-                               , [0, 1, 2, 0, 0, 0, 0]
-                               , [0, 1, 0, 2, 0, 0, 0]
-                               , [0, 1, 0, 0, 2, 0, 0]
-                               , [0, 1, 0, 0, 0, 0, 0]
-                               , [0, 0, 0, 0, 0, 0, 0]
-                               ]
-                in compare (sb b1) (sb b2) `shouldBe` GT
+    describe "empty" $
+        it "return a board state present empty" 
+            pending
 
         
 
